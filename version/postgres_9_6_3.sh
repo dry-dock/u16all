8d07e6c18b81f47478753eb829234e631a63d8db
raksha78 <rakshitha27@gmail.com>
2017-07-11 18:10:44 +0530

    readd the comment back:

--- version/postgres_9_6_3.sh
+++ version/postgres_9_6_3.sh
@@ -9,6 +9,10 @@ sudo apt-get update
 sudo apt-get install -y postgresql-9.6 postgresql-server-dev-9.6
 sudo apt-get install postgis
 
+# Fix bug https://github.com/docker/docker/issues/783
+# which prevents postgresql from staring when using aufs
+# This workaround proposed in the comment to the issue: https://github.com/docker/docker/issues/783#issuecomment-56013588
+# I've test this on my own builds
 mkdir /etc/ssl/private-copy
 mv /etc/ssl/private/* /etc/ssl/private-copy/
 rm -r /etc/ssl/private

772f532af3321119d539a6e10d97b7e661bf66b7
Niranjan Godbole <niranjan8192@gmail.com>
2017-08-07 05:16:26 +0000

    update services for u16

--- version/rethinkdb_2_3.sh
+++ version/rethinkdb_2_3.sh
@@ -15,6 +15,6 @@ sudo apt-get install -y \
   libjemalloc-dev
 
 # Install RethinkDB
-wget http://download.rethinkdb.com/apt/pool/xenial/main/r/rethinkdb/rethinkdb_2.3.5~0xenial_amd64.deb
+wget http://download.rethinkdb.com/apt/pool/xenial/main/r/rethinkdb/rethinkdb_2.3.6~0xenial_amd64.deb
 sudo dpkg -i rethinkdb*.deb
 rm rethinkdb*.deb

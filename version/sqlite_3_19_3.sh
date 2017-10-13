bd36a6471b83cb51c896b28d38bae79684978453
Bharath Upadhya <1992bharath@gmail.com>
2017-10-13 11:34:24 +0530

    updates postgres, rabbitmq, redis, sqlite

--- version/sqlite_3_19_3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-echo "================= Installing SQLite 3.19.3 ==================="
-
-
-sudo add-apt-repository ppa:jonathonf/backports
-
-sudo apt-get update && sudo apt-get install sqlite3

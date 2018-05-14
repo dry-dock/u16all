3e598e77998574ace0983c6ac158d772ada1ddac
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-14 13:14:53 +0530

    Update sqlite_3_22.0.sh

--- version/sqlite_3_22.0.sh
+++ version/sqlite_3_22.0.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository ppa:jonathonf/backports
 sudo apt-get update && sudo apt-get install sqlite3="$SQLITE_VERSION"*
 
 pushd /tmp
-  wget https://www.sqlite.org/2017/sqlite-tools-linux-x86-3220000.zip
+  wget https://www.sqlite.org/2018/sqlite-tools-linux-x86-3220000.zip
   unzip -u sqlite-tools-linux-x86-3220000.zip
   cp -r sqlite-tools-linux-x86-3220000/. /usr/bin
   rm -rf sqlite-tools-linux-x86-3220000*

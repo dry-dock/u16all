86349714e07a2cfb6313b3c86554ebf324045ce9
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-07-11 15:18:53 +0530

    Update mongodb_4_0_0.sh

--- version/mongodb_4_0_0.sh
+++ version/mongodb_4_0_0.sh
@@ -3,7 +3,7 @@
 MONGO_VERSION=4.0.0
 echo "================= Installing Mongodb $MONGO_VERSION ==================="
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 sudo apt-get update
